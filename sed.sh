f7587677c91c23f30e306857b1572b37c86c823d
Bima P <45479636+bimsky@users.noreply.github.com>
2020-12-13 10:27:04 +0700

    Update sed.sh

--- sed.sh
+++ sed.sh
@@ -42,8 +42,8 @@ fi
 x="$(basename $PWD)"
 
 echo "setup remote..."
-git remote add main git@gitlab.com:bimaguava/"$x".git
-git remote add origin https://gitlab.com/bimaguava/"$x"
+git remote add main git@gitlab.com:bimsky/"$x".git
+git remote add origin https://gitlab.com/bimsky/"$x"
 echo "[OK]"
 
 echo "publishing..."
@@ -69,7 +69,7 @@ isInFile=$(cat ../../../../.gitmodules | grep -c ""$(basename $PWD)"")
 
 if [ $isInFile -eq 0 ]; then
    cd ../../../../
-   git submodule add git@gitlab.com:bimaguava/"$x".git source/space/Import/"$x"
+   git submodule add git@gitlab.com:bimsky/"$x".git source/space/Import/"$x"
    echo "submodule updated..."
    git config --file .gitmodules --name-only --get-regexp path
 else
@@ -82,7 +82,7 @@ cd ../../../../
 #### if any error with submodule, run
 git submodule update --init
 git rm --cached source/space/Import/"$x"
-git submodule add git@gitlab.com:bimaguava/"$x".git source/space/Import/"$x"
+git submodule add git@gitlab.com:bimsky/"$x".git source/space/Import/"$x"
 git submodule update --init
 
 git add --all
